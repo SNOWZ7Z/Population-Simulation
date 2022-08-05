cf8d91f2d8ce92632b62ddeb13bd11c72c72f5b1
Marcelo Geldres <geldres.marcelo@gmail.com>
2022-08-05 15:03:42 -0400

    Environment Created

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as ani
+
+fig = plt.figure()
+axes = fig.add_subplot(111, projection="3d")
+axes.grid(False)
+
+plt.show()
+
